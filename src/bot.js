96afd9dfc1b4622d96077060581a895a0266020d
Marcio Tiene <marcio.ricardo@mettzer.com>
2021-11-07 00:09:16 -0300

    separate concerns

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,8 @@
 const dotenv = require("dotenv");
 const { Client, Intents } = require("discord.js");
 
+const {kickMember, banMember} = require('./commands')
+
 dotenv.config();
 
 const client = new Client({
@@ -19,35 +21,17 @@ client.on("messageCreate", (message) => {
     const [CMD_NAME, ...args] = message.content
       .substring(PREFIX.length)
       .split(/\s+/);
-    if (CMD_NAME === "kick") {
-      if (message.member.permissions.has("KICK_MEMBERS"))
-        return message.reply(
-          "Vilão, melhor ir parando por aqui, você não tem permissões, fraga?"
-        );
-      if (args.length === 0)
-        return message.reply("Preciso de um ID válido meu chegado");
-      const member = message.guild.members.cache.get(args[0]);
 
-      if (member) {
-        member.kick().then((member) => {
-          message.channel.send(`${member} foi de base`).catch((err) => {
-            message.channel.send(
-              "Vilão, deu ruim, conseguir banir o menor não"
-            );
-          });
-        });
-      } else {
-        message.channel.send("Não encontrei o membro não meu glorioso Jedi");
-      }
-    } else if (CMD_NAME === "ban") {
-      if (message.member.permissions.has("BAN_MEMBERS"))
-        return message.reply(
-          "Vilão, melhor ir parando por aqui, você não tem permissões, fraga?"
-        );
-      if (args.length === 0)
-        return message.reply("Preciso de um ID válido meu chegado");
-      message.guild.members.ban(args[0]).catch((err) => console.log(err));
-    }
+
+    switch(CMD_NAME){
+      case 'kick':
+        kickMember({message,args});
+        break;
+
+      case 'ban': 
+        banMember({message,args});
+        break;
+    }    
   }
 });
 
